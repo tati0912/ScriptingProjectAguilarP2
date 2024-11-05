ea80fab392de3a7fdec2ee2ecc97ded32616befa
tati0912 <730124@student.dusd.net>
2024-11-05 09:59:29 -0800

    finished 2 thing and didnt start adimations
    finished ifstatement, didnt start ademations, and finished anotherclass and scopeandaccessmodifiers

---
+++ ScriptingProjectP2Aguilar/Assets/Scripts/AnotherClass.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherClass : MonoBehaviour
+{
+    public int apples;
+    public int bananas;
+
+    private int stapler;
+    private int sellotape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void FruitMachine (int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Office Supplies total: " + answer);
+    }
+}
